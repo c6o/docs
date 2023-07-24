713cc4f5c0ab5b3eee751a709b796dfa9fae9fd3
Narayan Sainaney <narayan@codezero.io>
2023-07-24 14:45:15 -0700

    Updated theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,6 +41,7 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve("./sidebars.js"),
+          sidebarCollapsible: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
@@ -60,8 +61,8 @@ const config = {
       image: "img/docusaurus-social-card.jpg",
       docs: {
         sidebar: {
-          hideable: true,
-          autoCollapseCategories: true,
+          hideable: false,
+          // autoCollapseCategories: true,
         },
       },
       navbar: {
