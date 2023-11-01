bcb0e2959ae3ed4d02c8ff2a9372c73a5495376a
Georg Friedrich <georg@codezero.io>
2023-11-01 15:05:51 +1100

    chore: update tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Codezero Documentation",
-  tagline: "Dinosaurs are cool",
+  tagline: "the joy of flow",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
