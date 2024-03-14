d1d6e62ab5e43feeb8e3749f44d6ffb155b52775
Georg Friedrich <georg@codezero.io>
2024-03-14 17:04:30 +1100

    chore: add Algolia search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -109,6 +109,35 @@ const config = {
           // },
         ],
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'J877LJFWCO',
+
+        // Public API key: it is safe to commit it
+        apiKey: '351a6de2c5da7124507636d49357483e',
+
+        indexName: 'codezero',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: '/docs/', // or as RegExp: /\/docs\//
+        //   to: '/',
+        // },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        //... other Algolia params
+      },
       footer: {
         style: "dark",
         // links: [
