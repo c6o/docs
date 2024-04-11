73a42af2ad443e200f0e1fd156f1f44a2950d4b1
Georg Friedrich <georg@codezero.io>
2024-04-12 08:20:00 +1000

    Add 2.1.1 release notes with update instructions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
           lastVersion: "current",
           versions: {
             current: {
-              label: "Pasley (2.0)",
+              label: "Pasley (2.1)",
             },
             "1.9.0": {
               label: "Anvil (1.9)",
