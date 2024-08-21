64f0fd792e7d4f0cd8ef4b6d4673701092888fde
Georg Friedrich <georg@codezero.io>
2024-08-21 10:16:07 +1000

    chore: update references from 2.5.2 to 2.6.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
           lastVersion: "current",
           versions: {
             current: {
-              label: "v2.5",
+              label: "v2.6",
             },
           },
           routeBasePath: "/",
